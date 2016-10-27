cd25deae972aab20c5445232d122c87267c3b05f
beriks4 <beriks4@gmail.com>
2016-10-27 14:20:03 +0300

    client1 complete

--- Client1/js/script.js
+++ Client1/js/script.js
@@ -12,13 +12,15 @@ var cartCostElement = document.getElementById('cartCost');
 var cartRecipes = [];
 var recipesElement = document.getElementById('recipes');
 var addBtn = document.getElementById('addBtn');
-var addName = document.getElementById('addName');
-var addCost = document.getElementById('addCost');
+var cancelBtn = document.getElementById('cancelBtn');
+var nameInput = document.getElementById('nameInput');
+var costInput = document.getElementById('costInput');
 var data = {};
 
 productsElement.addEventListener('click', actionChooser);
 cartElement.addEventListener('click', actionChooser);
 addBtn.addEventListener('click', actionChooser);
+cancelBtn.addEventListener('click', tableRender);
 
 var products = JSON.parse(getRest(shopServer, shopTable)); //server answer
 var recipes = JSON.parse(getRest('recipes', 'dish'));
@@ -52,8 +54,7 @@ function putRest(server, table, data, id) {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
     xhr.open('PUT', 'http://rest/' + server + '/index.php/' + table + '/' + id, false);
-    xhr.data = data;
-    xhr.send();
+    xhr.send(JSON.stringify(data));
 }
 
 function putInTable(items) {
@@ -68,7 +69,7 @@ function putInTable(items) {
 
         addToCartBtn.className = 'btn btn-success col-lg-3 cartBtn';
         addToCartBtn.type = 'button';
-        addToCartBtn.value = 'В корзину';
+        addToCartBtn.value = 'To cart';
         addToCartBtn.id = items[i].id;
 
         deleteBtn.className = 'btn btn-danger col-lg-3 col-lg-offset-1 deleteBtn';
@@ -94,43 +95,80 @@ function putInTable(items) {
     }
 }
 
+function toCartAction(id) {
+    for (var i = 0; i < products.valueOf().length; i++) {
+        if (products[i].id == id ) {
+            cart.push(products[i]);
+        }
+    }
+}
+
+function outCartAction(id) {
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].id == id) {
+            cart.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function editAction(id) {
+    for (var i = 0; i < products.valueOf().length; i++) {
+        if (products[i].id == id) {
+            data = {
+                id: products[i].id,
+                name: products[i].name,
+                cost: products[i].cost
+            };
+            break;
+        }
+    }
+    addBtn.value = 'UPDATE';
+    addBtn.id = 'updateBtn';
+    nameInput.value = data.name;
+    costInput.value = data.cost;
+}
+
+function addAction() {
+    data = {
+        name: nameInput.value,
+        cost: costInput.value
+    };
+
+    postRest(shopServer, shopTable, data);
+}
+
+function updateAction() {
+    data = {
+        id: data.id,
+        name: nameInput.value,
+        cost: costInput.value
+    };
+
+    putRest(shopServer, shopTable, data, data.id);
+}
+
 function actionChooser(event) {
     var id = event.target.id;
-    //toCart
     if (event.target.className.indexOf('cartBtn') + 1) {
-        for (var i = 0; i < products.valueOf().length; i++) {
-            if (products[i].id == id ) {
-                cart.push(products[i]);
-            }
-        }
+        toCartAction(id);
     }
-    //outCart
     if (event.target.tagName === 'SPAN') {
-        for (var j = 0; j < cart.length; j++) {
-            if (cart[j].id == id) {
-                cart.splice(j, 1);
-                break;
-            }
-        }
+        outCartAction(id);
     }
-    //delete
     if (event.target.className.indexOf('deleteBtn') + 1) {
         deleteRest(shopServer, shopTable, id);
         tableRender();
     }
-    //edit
     if (event.target.className.indexOf('editBtn') + 1) {
-        putRest(shopServer, shopTable, null, null);
+        editAction(id);
+    }
+    if (event.target.id.indexOf('addBtn') + 1) {
+        addAction();
         tableRender();
     }
-    //add
-    if (event.target.className.indexOf('addBtn') + 1) {
-        data = {
-            name: addName.value,
-            cost: addCost.value
-        };
-        postRest(shopServer, shopTable, data);
-        data = {};
+    if (event.target.id.indexOf('updateBtn') + 1) {
+        updateAction();
         tableRender();
     }
 
@@ -168,7 +206,14 @@ function recipeRender () {
 
 function tableRender () {
     products = JSON.parse(getRest(shopServer, shopTable)); //server answer
+
     productsElement.innerHTML = '';
+    addBtn.value = 'ADD';
+    addBtn.id = 'addBtn';
+    nameInput.value = '';
+    costInput.value = '';
+    data = {};
+
     putInTable(products);
 }
 
